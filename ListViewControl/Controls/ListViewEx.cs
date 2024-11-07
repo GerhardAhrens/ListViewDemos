f61e141dc47233e07a10983e0734b255b08ae128
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-11-07 13:40:40 +0100

    *

--- ListViewControl/Controls/ListViewEx.cs
+++ ListViewControl/Controls/ListViewEx.cs
@@ -8,6 +8,7 @@
     using System.Windows.Documents;
     using System.Windows.Input;
     using System.Windows.Media;
+    using System.Windows.Shapes;
 
     public class ListViewEx : ListView
     {
@@ -40,7 +41,12 @@
             this.Loaded += this.OnLoaded;
             this.MouseDoubleClick += this.OnMouseDoubleClick;
             this.KeyDown += this.OnKeyDown;
+            this.AlternationCount = 2;
             this.SelectionChanged += this.OnSelectionChanged;
+            this.SetValue(KeyboardNavigation.IsTabStopProperty, false);
+            this.SetValue(ScrollViewer.IsDeferredScrollingEnabledProperty, false);
+            this.SetValue(VirtualizingPanel.IsVirtualizingProperty, true);
+            this.SetValue(VirtualizingPanel.VirtualizationModeProperty, VirtualizationMode.Recycling);
         }
 
         ~ListViewEx()
@@ -52,6 +58,11 @@
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
         }
 
         private void OnMouseDoubleClick(object sender, RoutedEventArgs e)
@@ -428,5 +439,83 @@
         }
 
         #endregion
+    }
+
+    public class GridViewColumnLines : GridViewRowPresenter
+    {
+        public static readonly DependencyProperty SeparatorStyleProperty;
+        private static readonly Style DefaultSeparatorStyle;
+        private readonly List<FrameworkElement> lines = new List<FrameworkElement>();
+
+        static GridViewColumnLines()
+        {
+            DefaultSeparatorStyle = new Style(typeof(Rectangle));
+            DefaultSeparatorStyle.Setters.Add(new Setter(Shape.FillProperty, SystemColors.ControlLightBrush));
+            SeparatorStyleProperty = DependencyProperty.Register("SeparatorStyle", typeof(Style), typeof(GridViewColumnLines),
+                                                                    new UIPropertyMetadata(DefaultSeparatorStyle, SeparatorStyleChanged));
+        }
+
+        public Style SeparatorStyle
+        {
+            get { return (Style)GetValue(SeparatorStyleProperty); }
+            set { this.SetValue(SeparatorStyleProperty, value); }
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return base.VisualChildrenCount + this.lines.Count; }
+        }
+
+        private IEnumerable<FrameworkElement> Children
+        {
+            get { return LogicalTreeHelper.GetChildren(this).OfType<FrameworkElement>(); }
+        }
+
+        protected override Size ArrangeOverride(Size arrangeSize)
+        {
+            var size = base.ArrangeOverride(arrangeSize);
+            var children = this.Children.ToList();
+            this.EnsureLines(children.Count);
+            for (var i = 0; i < this.lines.Count; i++)
+            {
+                var child = children[i];
+                var x = child.TransformToAncestor(this).Transform(new Point(child.ActualWidth, 0)).X + child.Margin.Right;
+                var rect = new Rect(x, -Margin.Top, 1, size.Height + Margin.Top + Margin.Bottom);
+                var line = this.lines[i];
+                line.Measure(rect.Size);
+                line.Arrange(rect);
+            }
+
+            return size;
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            var count = base.VisualChildrenCount;
+            return index < count ? base.GetVisualChild(index) : this.lines[index - count];
+        }
+
+        private static void SeparatorStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var presenter = (GridViewColumnLines)d;
+            var style = (Style)e.NewValue;
+            foreach (FrameworkElement line in presenter.lines)
+            {
+                line.Style = style;
+            }
+        }
+
+        private void EnsureLines(int count)
+        {
+            count = count - this.lines.Count;
+            for (var i = 0; i < count; i++)
+            {
+                var line = (FrameworkElement)Activator.CreateInstance(this.SeparatorStyle.TargetType);
+                line = new Rectangle { Fill = Brushes.LightGray };
+                line.Style = this.SeparatorStyle;
+                this.AddVisualChild(line);
+                this.lines.Add(line);
+            }
+        }
     }
 }
